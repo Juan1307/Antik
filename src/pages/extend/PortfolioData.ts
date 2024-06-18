051921b85b2a082e50c65cefe574e74efa2ff9dd
antik <antik@git.com>
2024-06-17 23:48:49 -0500

    add new changes

--- src/pages/extend/PortfolioData.ts
+++ src/pages/extend/PortfolioData.ts
@@ -1,135 +1,144 @@
 const projects = [
     {
-      title:'SYS ARCHIVO', 
-      address:'Ugel - Interno', 
-      text:'Sistema de gestión de resoluciones para  profesores y I.E.E',
-      src:['sys-arc-0.JPG','sys-arc-1.JPG','sys-arc-2.JPG'],
-      icons:['php','jquery','javascript', 'bootstrap']
-    },
-    { 
-      title:'SYS RECIBOS', 
-      address:'Ugel - Interno', 
-      text:'Sistema de control y gestión de recibos para las I.E.E',
-      src:['sys-rec-0.JPG','sys-rec-1.JPG','sys-rec-2.JPG'],
-      icons:['php','vuejs', 'bootstrap', 'nodejs']
+        title: 'SYS ARCHIVO',
+        address: 'Ugel - Interno',
+        text: 'Sistema de gestión de resoluciones para  profesores y I.E.E',
+        src: ['sys-arc-0.JPG', 'sys-arc-1.JPG', 'sys-arc-2.JPG'],
+        icons: ['php', 'jquery', 'javascript', 'bootstrap']
     },
     {
-      title:'SYS I.E.E', 
-      address:'Ugel - Administrativo', 
-      text:'Sistema de acceso y control para I.E.E y Directores.',
-      src:['sys-ie-0.JPG','sys-ie-1.JPG','sys-ie-2.JPG','sys-ie-3.JPG'],
-      icons:['php','vuejs', 'bootstrap', 'nodejs'],
+        title: 'SYS RECIBOS',
+        address: 'Ugel - Interno',
+        text: 'Sistema de control y gestión de recibos para las I.E.E',
+        src: ['sys-rec-0.JPG', 'sys-rec-1.JPG', 'sys-rec-2.JPG'],
+        icons: ['php', 'vuejs', 'bootstrap', 'nodejs']
     },
     {
-      title:'Audio App', 
-      address:'Personal', 
-      text:'Reproductor de audio con lista de canciones y patrón de lineas.',
-      src:['app-audio-0.JPG','app-audio-1.JPG','app-audio-2.JPG'],
-      icons:['html5','css3', 'javascript'],
-      link:'https://juan1307.github.io/AudioApp/'
+        title: 'SYS I.E.E',
+        address: 'Ugel - Administrativo',
+        text: 'Sistema de acceso y control para I.E.E y Directores.',
+        src: ['sys-ie-0.JPG', 'sys-ie-1.JPG', 'sys-ie-2.JPG', 'sys-ie-3.JPG'],
+        icons: ['php', 'vuejs', 'bootstrap', 'nodejs'],
+    },
+
+    {
+        title: 'SYS Proveedores',
+        address: 'HLC - Administrativo',
+        text: 'Sistema de evaluación, administración y gestión de proveedores.',
+        src: ['sys-pro-0.png', 'sys-pro-1.png', 'sys-pro-2.png'],
+        icons: ['php', 'react', 'bootstrap', 'nodejs'],
+    },
+
+    {
+        title: 'Audio App',
+        address: 'Personal',
+        text: 'Reproductor de audio con lista de canciones y patrón de lineas.',
+        src: ['app-audio-0.JPG', 'app-audio-1.JPG', 'app-audio-2.JPG'],
+        icons: ['html5', 'css3', 'javascript'],
+        link: 'https://juan1307.github.io/AudioApp/'
     },
     {
-      title:'Acui Game', 
-      address:'Personal', 
-      text:'Juego 2D, con el fin de concientizar el cuidado del océano.',
-      src:['app-acui-0.JPG','app-acui-1.JPG','app-acui-2.JPG'],
-      icons:['html5','css3', 'javascript', 'tailwind-css'],
-      link:'https://juan1307.github.io/AcuiApp/'
+        title: 'Acui Game',
+        address: 'Personal',
+        text: 'Juego 2D, con el fin de concientizar el cuidado del océano.',
+        src: ['app-acui-0.JPG', 'app-acui-1.JPG', 'app-acui-2.JPG'],
+        icons: ['html5', 'css3', 'javascript', 'tailwind-css'],
+        link: 'https://juan1307.github.io/AcuiApp/'
     },
     {
-      title:'Open User', 
-      address:'Personal', 
-      text:'Demo Blog de usuarios, integrado con datos públicos.',
-      src:['app-blog-0.JPG','app-blog-1.JPG','app-blog-2.JPG'],
-      icons:['react', 'javascript', 'bootstrap'],
-      link:'https://juan1307.github.io/OpenUser/'
+        title: 'Open User',
+        address: 'Personal',
+        text: 'Demo Blog de usuarios, integrado con datos públicos.',
+        src: ['app-blog-0.JPG', 'app-blog-1.JPG', 'app-blog-2.JPG'],
+        icons: ['react', 'javascript', 'bootstrap'],
+        link: 'https://juan1307.github.io/OpenUser/'
     }];
 
 const platforms = [
     {
-      title:'Git y GitHub', 
-      address:'Sistema Control de versiones', 
-      text:'Control y gestion de repositorios en github.',
-      src:'pla-github.JPG',
-      icons:['github','git']
+        title: 'Git y GitHub',
+        address: 'Sistema Control de versiones',
+        text: 'Control y gestion de repositorios en github.',
+        src: 'pla-github.JPG',
+        icons: ['github', 'git']
     },
     {
-      title:'Trello', 
-      address:'Flujo de trabajo y Desarrollo', 
-      text:'Control de procesos de desarrollo para Aplicaciones.',
-      src:'pla-trello.JPG',
-      icons:['trello']
+        title: 'Trello',
+        address: 'Flujo de trabajo y Desarrollo',
+        text: 'Control de procesos de desarrollo para Aplicaciones.',
+        src: 'pla-trello.JPG',
+        icons: ['trello']
     },
     {
-      title:'Heroku', 
-      address:'Despliegue de Aplicaciones', 
-      text:'Hosting para Aplicaciones Front-End y Back-End.',
-      src:'pla-heroku.JPG',
-      icons:['heroku']
+        title: 'Heroku',
+        address: 'Despliegue de Aplicaciones',
+        text: 'Hosting para Aplicaciones Front-End y Back-End.',
+        src: 'pla-heroku.JPG',
+        icons: ['heroku']
     },
     {
-      title:'Star UML', 
-      address:'Modelo de Aplicaciones', 
-      text:'Lenguaje de modelado para procesos de desarrollo.',
-      src:'pla-staruml.JPG'
+        title: 'Star UML',
+        address: 'Modelo de Aplicaciones',
+        text: 'Lenguaje de modelado para procesos de desarrollo.',
+        src: 'pla-staruml.JPG'
     },
     {
-      title:'Balsamiq', 
-      address:'Prototipado de Aplicaciones', 
-      text:'Diseño y prototipado de aplicaciones.',
-      src:'pla-balsamiq.JPG'
+        title: 'Balsamiq',
+        address: 'Prototipado de Aplicaciones',
+        text: 'Diseño y prototipado de aplicaciones.',
+        src: 'pla-balsamiq.JPG'
     }];
 
 const scripts = [
-  {
-    title:'UI categorias',
-    address:'Codepen',
-    text:'Diseño Ui con tailwind css.',
-    src:'src-categorias.JPG',
-    icons:['html5','tailwind-css'],
-    link:'https://codepen.io/antik133up/pen/abWNNML'
-  },
-  {
-    title:'CSS animaciones',
-    address:'Codepen',
-    text:'Animaciones a cajas con css.',
-    src:'src-animaciones.JPG',
-    icons:['html5','css3'],
-    link:'https://codepen.io/antik133up/pen/MWpmYVZ'
-  },
-  {
-    title:'UI cartas pokemón',
-    address:'Codepen',
-    text:'Diseño de Cartas pokemón.',
-    src:'src-pokemon.JPG',
-    icons:['html5','css3'],
-    link:'https://codepen.io/antik133up/pen/VwPJzYQ'
-  },
-  {
-    title:'Usuarios Api',
-    address:'Codepen',
-    text:'Renderizado de Comentarios con Api.',
-    src:'src-comentarios.JPG',
-    icons:['html5','css3','javascript'],
-    link:'https://codepen.io/antik133up/pen/QWpwVVZ'
-  },
-  {
-    title:'Galeria de Imágenes',
-    address:'Codepen',
-    text:'Renderizado de imágenes con Api.',
-    src:'src-galeria.JPG',
-    icons:['html5','css3','javascript'],
-    link:'https://codepen.io/antik133up/pen/VwPGKvr'
-  },
-  {
-    title:'Web Portfolio',
-    address:'github',
-    text:'Landing Page UX de prueba.',
-    src:'src-portfolio.JPG',
-    icons:['html5','css3','javascript'],
-    link:'https://juan1307.github.io/WebPortfolio/'
-  }];
-  
-const PortfolioData = [ projects, platforms, scripts ];
+    {
+        title: 'UI categorias',
+        address: 'Codepen',
+        text: 'Diseño Ui con tailwind css.',
+        src: 'src-categorias.JPG',
+        icons: ['html5', 'tailwind-css'],
+        link: 'https://codepen.io/antik133up/pen/abWNNML'
+    },
+    {
+        title: 'CSS animaciones',
+        address: 'Codepen',
+        text: 'Animaciones a cajas con css.',
+        src: 'src-animaciones.JPG',
+        icons: ['html5', 'css3'],
+        link: 'https://codepen.io/antik133up/pen/MWpmYVZ'
+    },
+    {
+        title: 'UI cartas pokemón',
+        address: 'Codepen',
+        text: 'Diseño de Cartas pokemón.',
+        src: 'src-pokemon.JPG',
+        icons: ['html5', 'css3'],
+        link: 'https://codepen.io/antik133up/pen/VwPJzYQ'
+    },
+    {
+        title: 'Usuarios Api',
+        address: 'Codepen',
+        text: 'Renderizado de Comentarios con Api.',
+        src: 'src-comentarios.JPG',
+        icons: ['html5', 'css3', 'javascript'],
+        link: 'https://codepen.io/antik133up/pen/QWpwVVZ'
+    },
+    {
+        title: 'Galeria de Imágenes',
+        address: 'Codepen',
+        text: 'Renderizado de imágenes con Api.',
+        src: 'src-galeria.JPG',
+        icons: ['html5', 'css3', 'javascript'],
+        link: 'https://codepen.io/antik133up/pen/VwPGKvr'
+    },
+    {
+        title: 'Web Portfolio',
+        address: 'github',
+        text: 'Landing Page UX de prueba.',
+        src: 'src-portfolio.JPG',
+        icons: ['html5', 'css3', 'javascript'],
+        link: 'https://juan1307.github.io/WebPortfolio/'
+    }];
+
+const PortfolioData = [projects, platforms, scripts];
 
 export default PortfolioData
